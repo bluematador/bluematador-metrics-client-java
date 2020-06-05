1abf2d873c091819a8cfcd97c37cc84c2cdae144
Hunter <hunter@bluematador.com>
2020-06-05 11:20:33 -0600

    added note to client.java

--- metrics-exporter/src/main/java/com/bluematador/app/Client.java
+++ metrics-exporter/src/main/java/com/bluematador/app/Client.java
@@ -4,6 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
+// This file is soley used to test sending custom metrics to the Agent.
+// It will not be included when we are ready to publish to Maven
+
 public class Client {
     private static final Logger logger = LogManager.getLogger(Client.class);
 
